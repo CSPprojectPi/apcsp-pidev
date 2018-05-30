831f4f2ecc58c6129c918b8e756303a2b6d4f3d7
csp <csp.austin.sumit@gmail.com>
2018-05-30 15:05:53 -0700

    test

--- cdemo/userInput_Circle.c
+++ cdemo/userInput_Circle.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 
-float areaOfCircle(float r){
+float areaOfCircle(float radius1){
 
 	float area = 0;
-	area = 3.14*r*r;
+	area = 3.14*radius1*radius1;
 	return area;
 }
 
 void main(void){
 
 	float mainArea = 0;
-	float r;
+	char r1[256];
+	char r2[256];
+	float radius1;
+	float radius2;
+
 	printf("Enter the first value of the radius\n");
-	fgets(r, );
-	for(r = 3.5; r <= 12.5; r++){
-		mainArea = areaOfCircle(r);
-		printf("the area of the circle with r = %f is %f\n", r, mainArea);
+
+	while (1)
+	{
+		fgets(r1, 256, stdin);
+		if (sscanf(r1, "%f", &radius1) == 1)
+			break;
+		else
+			printf("enter a number\n");
+	}
+
+	printf("Enter the second value of the radius\n");
+
+	fgets(r2, 256, stdin);
+	sscanf(r2, "%f", &radius2);
+
+	for(radius1; radius1 <= radius2; radius1++)
+	{
+		mainArea = areaOfCircle(radius1);
+		printf("the area of the circle with r = %f is %f\n", radius1, mainArea);
 	}
 }
