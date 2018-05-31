8141935ef153610064aa5d4890912820af3d0e29
CSPprojectPi <38962502+CSPprojectPi@users.noreply.github.com>
2018-05-31 14:42:20 -0700

    test

--- cdemo/String.c
+++ cdemo/String.c
@@ -5,18 +5,19 @@ void main() {
   char alphbet1[27];
   char alphbet2[] = "abcdefghijklmnopqrstuvwxyz";
   char alphbet3[27];
+  char alphbet4[27];
+  
 
   printf ("alphbet 1: %s\n", alphbet2);
-  alphbet1[0] = 'a';
+  char c = 'a';
 
   printf ("alphbet 2: ");
-  printf ("%c", alphbet1[0]);
-  for (int i = 1; i < 26; i++) {
-    alphbet1[i] = alphbet1[i-1] + 1;
-    printf ("%c", alphbet1[i]);
+  for (int i = 0; i < 26; i++) {
+    alphbet1[i] = c;
+    c++;
   }
   alphbet1[26] = '\0';
-  printf ("\n");
+  printf ("alphbet 1 = %s\n", alphbet1);
 
   if (strcmp(alphbet1, alphbet2) == 0) {
   printf ("alphbet1 and alphbet2 are the same\n");
@@ -24,12 +25,20 @@ void main() {
   printf ("alphbet1 and alphbet2 are not the same\n");
   }
 
+  char d = 'A';
+
   printf ("uppercase alphbet: ");
   for (int i = 0; i < 26; i++) {
-    alphbet3[i] = alphbet1[i] - 32;
-    printf ("%c", alphbet3[i]);
+    alphbet3[i] = d;
+    d++;
   }
+
   alphbet3[26] = '\0';
+  printf("%s", alphbet3);
+  
   printf ("\n");
 
+  strcat(alphbet4, alphbet1);
+  strcat(alphbet4, alphbet3);
+  printf("combined: %s", alphbet4);
 }
