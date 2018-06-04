9102000c3eac329cd6f112287b3ba62767f88f98
CSPprojectPi <38962502+CSPprojectPi@users.noreply.github.com>
2018-06-04 14:54:28 -0700

    Update calculator.c

--- CprogramsOfSumit/calculator.c
+++ CprogramsOfSumit/calculator.c
@@ -15,31 +15,41 @@ float division(float c, float d){
 
 int main(){
 
-	float mulAns1 = 0;
-	char x[256];
-	char y[256];
-	float a;
-	float b;
+	char symbol;
 	
-	printf("Enter the first number you want to multiply:\n");
-	while (1){
-		fgets(x, 256, stdin);
-		if (sscanf(x, "%f", &a) == 1)
-			break;
-		else
-			printf("It needs to be a number!\n");
-	}
-	printf("Enter the second number: \n");
-	while (1)
-	{
-		fgets(y, 256, stdin);
-		if (sscanf(y, "%f", &b) == 1)
-			break;
-		else
-			printf("It needs to be a number!\n");
-	}
+	printf("Enter the symbol of the math operation you want to perform:\n");
+	printf("For Multiplication, type '*'\n");
+	printf("For Division, type '/'\n");
+	fgets(symbol, 1, stdin);
+	sscanf(symbol, "%c", &symbol); //need to include loop for making sure the user enters only one of the symbols displayed above
+	
+	if(symbol = *){
+		float mulAns1 = 0;
+		char x[256];
+		char y[256];
+		float a;
+		float b;
+	
+		printf("Enter the first number you want to multiply:\n");
+		while (1){
+			fgets(x, 256, stdin);
+			if (sscanf(x, "%f", &a) == 1)
+				break;
+			else
+				printf("It needs to be a number!\n");
+		}
+		printf("Enter the second number: \n");
+		while (1)
+		{
+			fgets(y, 256, stdin);
+			if (sscanf(y, "%f", &b) == 1)
+				break;
+			else
+				printf("It needs to be a number!\n");
+		}
 
-	mulAns1 = multiplication(a, b);
-	printf("The product of %f and %f is %f.\n", a, b, mulAns1);
+		mulAns1 = multiplication(a, b);
+		printf("The product of %f and %f is %f.\n", a, b, mulAns1);
+	}
 }
 
